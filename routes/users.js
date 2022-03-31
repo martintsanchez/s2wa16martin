4e61f4af00e7d0c89a66599ccb1fdda3bc29201c
Martin Sanchez <s534880@nwmissouri.edu>
2022-03-30 20:50:03 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,8 @@ var times = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  times.get('/', function(req, res, next) {  
   res.send(`User accesses are: ${times}`);
-});
+})});
 
 module.exports = router;
